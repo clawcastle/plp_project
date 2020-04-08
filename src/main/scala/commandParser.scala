e2cdffc5a8bcab8e88d6fbf20ea54e96f4c370b1
anderskloborg <anderskloborg@gmail.com>
2020-04-08 14:31:54 +0200

    Added another take on doing this in a functional way

--- src/main/scala/commandParser.scala
+++ src/main/scala/commandParser.scala
@@ -25,4 +25,40 @@ object commandParser {
     val params = command.split(" ").tail.map(x => x.toInt)
     draw.drawLine(params(0), params(1), params(2), params(3))
   }
+
+  def transformString(str: String, splitChar: Char, transformers: List[String => String]): List[String] = {
+    transformers match {
+      case Nil => str.split(splitChar).toList;
+      case t => transformString(t.head(str), splitChar, t.tail)
+    }
+  }
+
+  def parseCommands(commandsString: String): List[List[String]] = {
+    commandsString.split(sys.props("line.separator")).map(str => transformString(str, ' ', List(replaceParentheses))).toList
+  }
+
+  def mapToShapes(commands: String): Unit = {
+    parseCommands(commands).map(l => draw.drawLine(l))
+  }
+}
+
+class Shape {
+  val coordinates: List[Coordinates] = List[Coordinates]()
+}
+
+class Square extends Shape {
+
+}
+
+class Line extends Shape {
+
+}
+
+class Circle extends Shape {
+
+}
+
+class Coordinates {
+  val x: Int = 0
+  val y: Int = 0
 }
